3b7c6e413243e552af30307c332cb1dfd8b5d1d9
b633 <b633@borqs.com>
2014-03-20 18:53:18 +0800

    shan jiejing: 3.20 complete show all image.
    will do xor operation on two image.

--- showAllImg/main.c
+++ showAllImg/main.c
@@ -17,20 +17,23 @@ static const char * WINDOWNAME = "image window";
 int g_slider_position = 0;
 char* images[MAX_IMAGE_NUM];
 IplImage *g_image = NULL;
+char path[200] = "\0";
 
 void onTrackerbarSlide(int pos)
 {
+	char tmp[200] = "\0";
+	sprintf(tmp, "%s/%s", path, images[pos]);
 	if (g_image != NULL)
 		cvReleaseImage(&g_image);
-	g_image = cvLoadImage(images[pos], 1);
-	svShowImage(WINDOWNAME, g_image);
+	g_image = cvLoadImage(tmp, 1);
+	cvShowImage(WINDOWNAME, g_image);
 }
 /*static void showImage(IplImage *img)
 {
 	cvNamedWindow(WINDOWNAME, CV_WINDOW_AUTOSIZE);
 	cvShowImage(WINDOWNAME, img);
 }*/
-static int getAllImage(const char *dir)
+static int getAllImages(const char *dir)
 {
 	int image_num = 0;
 	DIR *di = opendir(dir);
@@ -40,12 +43,14 @@ static int getAllImage(const char *dir)
 	{
 		if (ptr->d_name[0] == '.')
 			continue;
-		if ((strcmp(ptr->d_name + ptr->d_reclen - 4, ".jpg") == 0) ||
-			(strcmp(ptr->d_name + ptr->d_reclen - 4, ".JPG") == 0))
+		if ((strcmp(ptr->d_name + strlen(ptr->d_name) - 4, ".jpg") == 0) ||
+			(strcmp(ptr->d_name + strlen(ptr->d_name) - 4, ".JPG") == 0))
+		{
 			image_num ++;
-		if (image_num > MAX_IMAGE_NUM) break;
-		images[image_num - 1] = malloc(ptr->d_reclen + 1);
-		strcpy(images[image_num - 1], ptr->d_name);
+			if (image_num > MAX_IMAGE_NUM) break;
+			images[image_num - 1] = malloc(ptr->d_reclen + 1);
+			strcpy(images[image_num - 1], ptr->d_name);
+		}
 	}
 	closedir(di);
 
@@ -65,22 +70,32 @@ int main (int argc, char** argv)
 		return 0;
 	}
 
+	int i = 0;
 	int imgnum = getAllImages(argv[1]);
+	strcpy(path, argv[1]);
+	path[strlen(argv[1]) - 1] = '\0';
+	printf("IMG NUM = %d\n", imgnum);
 
 	cvNamedWindow(WINDOWNAME, CV_WINDOW_NORMAL);
 	if (imgnum > 1)
 	{
 		cvCreateTrackbar("pic_num", WINDOWNAME, &g_slider_position, imgnum, onTrackerbarSlide);
 	}
-	g_image = cvLoadImage(images[0], 1);
+	for (i = 0; i < imgnum; i++)
+	{
+		printf("[%d] image name = %s\n", i, images[i]);
+	}
+	char tmp[200] = "\0";
+	sprintf(tmp, "%s/%s", path, images[0]);
+	printf("%s\n", tmp);
+	g_image = cvLoadImage(tmp, 1);
 	cvShowImage(WINDOWNAME, g_image);
 
 	cvWaitKey(0);
 	if (g_image != NULL)
 		cvReleaseImage(&g_image);
-	int i = 0;
-	for (i = 0; i < imgnum; i++)
-		free(images[i]);
+	//for (i = 0; i < imgnum; i++)
+	//	free(images[i]);
 	cvDestroyWindow(WINDOWNAME);
 
 	return 0;
---
+++ testXor/main.c
@@ -0,0 +1,24 @@
+/*
+ * read two image file in gray and do xor operation.
+ *
+ * author: shan jiejing
+ *
+ * 2014.3.20
+ */
+
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		help();
+		return 0;
+	}
+
+
+}
